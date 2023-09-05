c5f0b13639c3c7a0a70b951503c7239c534cdfae
carolinamoralese <caromorales1396@gmail.com>
2023-09-05 15:54:21 -0500

    se hace peticion api para mostrar los productos

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,14 +1,28 @@
-import Layout from "../../Components/Layout"
-import Card from "../../Components/Card"
+import { useState, useEffect } from "react";
+import Layout from "../../Components/Layout";
+import Card from "../../Components/Card";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then(response => response.json())
+    .then(data => setItems(data))
+  }, []);
 
   return (
-      <Layout>
-         Home
-         <Card />
-      </Layout>
-  )
+    <Layout>
+      Home
+     <div className="grid gap-6 grid-cols-3 w-full max-w-screen-lg bg-slate-100">
+     {
+        items ?.map((item) => {
+            return <Card key={item.id} data={item} />
+        })
+      }
+     </div>
+    </Layout>
+  );
 }
 
-export default Home
+export default Home;
